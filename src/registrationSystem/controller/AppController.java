2a58bb39e7e14254f8ceae708deb4e14a0677c79
Bhavyai Gupta <zbhavyai@gmail.com>
2021-10-06 20:00:26 -0600

    Adding AppController

---
+++ src/registrationSystem/controller/AppController.java
@@ -0,0 +1,55 @@
+package registrationSystem.controller;
+
+import registrationSystem.view.*;
+import registrationSystem.model.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AppController {
+    private AppView appView;
+    private CourseCatalogModel cat;
+    private StudentModel stu;
+
+    public AppController() {
+        this.setAppView(new AppView());
+        this.setActions();
+        this.getAppView().setVisible(true);
+    }
+
+    public AppView getAppView() {
+        return appView;
+    }
+
+    public void setAppView(AppView appView) {
+        this.appView = appView;
+    }
+
+    public void setActions() {
+        this.appView.actionOnEnterButton(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String studentName = appView.getStudentName();
+                int studentId = appView.getStudentId();
+
+                stu = new StudentModel(studentName, studentId);
+                cat = new CourseCatalogModel();
+
+                AppModel.getInstance(cat);
+
+                appView.setVisible(false);
+                new MenuController(cat, stu);
+            }
+        });
+
+        this.appView.actionOnQuitButton(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        new AppController();
+    }
+}
